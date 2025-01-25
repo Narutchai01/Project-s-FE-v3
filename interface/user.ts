d517dcbbda9ad0847a50458a30257b7f230f8456
Narutchai <koonfluk2015@gmail.com>
2025-01-25 22:51:31 +0700

    💄add ui components

--- interface/user.ts
+++ interface/user.ts
@@ -1,10 +1,12 @@
+import {Dayjs} from "dayjs";
+
 export interface ILogin{
     email: string;
     password: string;
 }
 export interface ISignUp{
     full_name: string;
-    birthday: Date | null;
+    birthday: Dayjs | null;
     email: string;
     password: string;
     sensitive_skin: boolean;
