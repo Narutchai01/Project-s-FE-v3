8eb4f5214489115e3469017333912a3a771d6251
nkmbxy <111549955+nkmbxy@users.noreply.github.com>
2025-02-09 00:56:49 +0700

    :lipstick: adjust format compare card on the compare screen

--- components/CompareDiary.tsx
+++ components/CompareDiary.tsx
@@ -8,9 +8,12 @@ interface CompareDiaryProps {
 
 export default function CompareDiary({ image, date }: CompareDiaryProps) {
   return (
-    <View className="items-center bg-white shadow-md p-2 rounded-lg mx-2 w-[113px] h-[186px]">
-      <Image source={{ uri: image }} className="w-[88px] h-[118px] rounded-md" />
-      <Text className="text-label2 mt-2">Date {date}</Text>
+    <View className="items-center bg-white shadow-md p-5 rounded-lg mx-2 w-[113px] h-[186px]">
+      <View className="w-[88px] h-[118px] rounded-md ">
+      <Image source={{ uri: image }} className="w-full h-full rounded-md object-cover " />
+      </View>
+      <Text className="text-label2 mt-2 self-start">Date</Text>
+      <Text className="text-label2 mb-2 self-start">{date}</Text>
     </View>
   );
 }
