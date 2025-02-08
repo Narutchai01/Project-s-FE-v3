84560e839dbdb2ff48d3cac3bc039aa78d5d6ab0
nkmbxy <111549955+nkmbxy@users.noreply.github.com>
2025-02-09 00:31:02 +0700

    :lipstick: update format text in popular skincare card

--- components/PopularSkincare.tsx
+++ components/PopularSkincare.tsx
@@ -36,13 +36,16 @@ export default function PopularSkincare() {
             </View>
 
             <View
-              className="absolute bottom-0 left-0 w-[118px] h-[30px] bg-white p-2 flex items-start justify-center"
+              className="absolute bottom-0 left-0 w-full h-[30px] bg-white px-2 flex items-start justify-center"
               style={{ borderTopLeftRadius: 13, borderTopRightRadius: 10 }}
             >
-                <Text className="text-label12 font-medium" numberOfLines={1}
-                  ellipsizeMode="tail">
-                  Granactive Retinoid 5%Granactive Retinoid 5%
-                </Text>
+              <Text
+                className="text-label12 font-medium w-full"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Granactive Retinoid 5% Serum for Anti-Aging
+              </Text>
             </View>
           </View>
         ))}
