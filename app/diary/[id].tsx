3f1bf5f37811c18a9b292f533c4254855c43684d
nkmbxy <111549955+nkmbxy@users.noreply.github.com>
2025-04-03 14:34:19 +0700

    :lipstick: update ui in result analysis

--- app/diary/[id].tsx
+++ app/diary/[id].tsx
@@ -187,14 +187,13 @@ const Section: React.FC<SectionProps> = ({ title, items }) => (
             style={{ width: 65, height: 65, borderRadius: 50 }}
           />
           <View className="flex items-center justify-center mt-2 mb-4">
-            <Text 
-             numberOfLines={1}
-                        ellipsizeMode="tail"
-                        style={{ width: 60, textAlign: "center"}}
-            className="text-label6"
+            <Text
+               numberOfLines={3} 
+               style={{ width: 60, textAlign: "center" }}
+              className="text-label6"
             >
               {item.name}
-              </Text>
+            </Text>
           </View>
         </View>
       ))}
