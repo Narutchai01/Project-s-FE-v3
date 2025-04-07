2974e69d66e0592b5807792d54d127955447ea8d
nkmbxy <111549955+nkmbxy@users.noreply.github.com>
2025-04-08 02:43:36 +0700

    change api

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-unresolved, @typescript-eslint/no-unused-vars
 
-export const api_url = "http://localhost:8080/api";
+export const api_url = "https://ucare.narutchai.com/api";
 
